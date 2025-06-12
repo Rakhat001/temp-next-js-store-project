8d7e46c947918bf9d393af95ef3877fb0f4b22b4
Rakhat001 <rakhat.ismailov.2001@gmail.com>
2025-06-12 18:03:36 +0500

    add clerk authentufucation

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { ThemeProvider } from "./theme-provider"
+import { Toaster } from "@/components/ui/toaster"
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange> 
+      <Toaster />
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         {children}
       </ThemeProvider>
 
